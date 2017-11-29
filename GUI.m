593727f1806b4e71878e6644d621ce556bfa3faa
Thomas Cassimon <thomas.cassimon@outlook.com>
2017-11-29 12:38:40 +0100

    Added data filtering

--- GUI.m
+++ GUI.m
@@ -22,7 +22,7 @@ function varargout = GUI(varargin)
 
 % Edit the above text to modify the response to help GUI
 
-% Last Modified by GUIDE v2.5 29-Nov-2017 11:30:26
+% Last Modified by GUIDE v2.5 29-Nov-2017 12:02:49
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -70,6 +70,8 @@ handles.settingsPanel = uitab('Parent', handles.tgroup, 'Title', 'Processing set
 
 %Place panels into each tab
 set(handles.dataTable, 'Parent', handles.dataPanel)
+addprop(handles.dataTable, 'selectedRows');
+addprop(handles.dataTable, 'selectedCols');
 set(handles.filterDataButton, 'Parent', handles.dataPanel)
 
 set(handles.rawData, 'Parent', handles.plotPanel)
@@ -82,15 +84,6 @@ set(handles.frequencyDomainTag, 'Parent', handles.plotPanel)
 
 set(handles.windowFunctionGroup, 'Parent', handles.settingsPanel)
 set(handles.noiseRemovalGroup, 'Parent', handles.settingsPanel)
-
-
-
-%plot test
-x = 0:10;
-y = 0:10;
-plot(x,y,handles.timeDomainPlot);
-
-
 end
 
 % UIWAIT makes GUI wait for user response (see UIRESUME)
@@ -225,6 +218,8 @@ set(handles.dataTable,'data',data);
 
 end
 
+%selectedRows = [];
+%selectedCols = [];
 
 % --- Executes on button press in filterDataButton.
 function filterDataButton_Callback(hObject, eventdata, handles)
@@ -232,4 +227,24 @@ function filterDataButton_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+selectedRows = handles.dataTable.selectedRows;
+selectedCols = handles.dataTable.selectedCols;
+data = get(handles.dataTable,'data');
+
+%fprintf('Min Row: %d, Max Row: %d\nMin Col: %d Max Col: %d',);
+data = data(min(selectedRows):max(selectedRows),min(selectedCols):max(selectedCols))
+set(handles.dataTable,'data',data);
+end
+
+
+% --- Executes when selected cell(s) is changed in dataTable.
+function dataTable_CellSelectionCallback(hObject, eventdata, handles)
+% hObject    handle to dataTable (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.TABLE)
+%	Indices: row and column indices of the cell(s) currently selecteds
+% handles    structure with handles and user data (see GUIDATA)
+
+
+handles.dataTable.selectedRows = unique(eventdata.Indices(:,1));
+handles.dataTable.selectedCols = unique(eventdata.Indices(:,2));
 end
