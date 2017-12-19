04f3da415c208827a80d6a7977c220aec924ac37
Thomas Cassimon <thomas.cassimon@outlook.com>
2017-12-19 14:43:18 +0100

    Made toString for equiation mayth channel

--- generateMathChannelString.m
+++ generateMathChannelString.m
@@ -1,6 +1,12 @@
 function [string] = generateMathChannelString(handles)
-%UNTITLED Summary of this function goes here
+%generateMathChannelString Generates a string that represents the equation
+%currently in the Math channel.
 %   Detailed explanation goes here
 
+	raw = handles.rawDataButtonGroup.SelectedObject.String
+	proc = handles.processedDataButtonGroup.SelectedObject.String
+
+	string = [raw ' raw data ' proc ' processed data'];
+	
 end
 
