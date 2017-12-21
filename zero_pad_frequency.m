e62a64128526a1f1e9fb97730a1ae3633de0c88d
Astrid Vanneste <astrid.vanneste1@gmail.com>
2017-12-21 18:08:37 +0100

    Delete zero_pad_frequency.m

--- zero_pad_frequency.m
+++
@@ -1,3 +0,0 @@
-function [result] = zero_pad_frequency (signal, desiredLength)
-    result = [zeros(1, floor((desiredLength - length(signal)) / 2)) signal zeros(1, ceil((desiredLength - length(signal)) / 2))];
-end
