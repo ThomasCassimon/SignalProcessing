45a20b038e2b1d050d5ed63c517bd14f3daaad5e
Astrid Vanneste <astrid.vanneste1@gmail.com>
2017-12-21 18:10:32 +0100

    Delete signal_info.m

--- signal_info.m
+++
@@ -1,9 +0,0 @@
-%   Signal Information Script
-%   Returns Peak-to-Peak value, mean, variance, RMS
-
-function [ptp, sigMean, sigVar, sigRMS] = signal_info (y)
-    ptp = max(y) - min(y);
-    sigMean = mean(y);
-    sigVar = mean((y - sigMean).^2);
-    sigRMS = rms(y);
-end
